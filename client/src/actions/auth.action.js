e0ed273d944632def8eeee38db81af750f15a7a7
Yuda <yuda10aditya@gmail.com>
2020-12-24 15:02:24 +0700

    create product page

--- client/src/actions/auth.action.js
+++ client/src/actions/auth.action.js
@@ -1,104 +1,74 @@
-import { authConstants} from "./constants";
+import { authConstants } from "./constants";
 import axios from "../helpers/axios";
 
-// new update signup action
-export const signup = (user) => {
-    return async (dispatch) => {
-        let res;
-        try {
-            dispatch({ type: authConstants.SIGNUP_REQUEST });
-            res = await axios.post(`/signup`, user);
-            if (res.status === 201) {
-                dispatch({ type: authConstants.SIGNUP_SUCCESS });
-                const { token, user } = res.data;
-                localStorage.setItem("token", token);
-                localStorage.setItem("user", JSON.stringify(user));
-                dispatch({
-                    type: authConstants.LOGIN_SUCCESS,
-                    payload: {
-                        token,
-                        user,
-                    },
-                });
-            } else {
-                const { error } = res.data;
-                dispatch({ type: authConstants.SIGNUP_FAILURE, payload: { error } });
-            }
-        } catch (error) {
-            const { data } = error.response;
-            dispatch({
-                type: authConstants.SIGNUP_FAILURE,
-                payload: { error: data.error },
-            });
-        }
-    };
-};
-
 export const login = (user) => {
+
+    console.log(user)
+
     return async (dispatch) => {
+
         dispatch({ type: authConstants.LOGIN_REQUEST });
         const res = await axios.post(`/signin`, {
-            ...user,
+            ...user
         });
 
         if (res.status === 200) {
             const { token, user } = res.data;
-            localStorage.setItem("token", token);
-            localStorage.setItem("user", JSON.stringify(user));
+            localStorage.setItem('token', token);
+            localStorage.setItem('user', JSON.stringify(user));
             dispatch({
                 type: authConstants.LOGIN_SUCCESS,
                 payload: {
-                    token,
-                    user,
-                },
+                    token, user
+                }
             });
         } else {
             if (res.status === 400) {
                 dispatch({
                     type: authConstants.LOGIN_FAILURE,
-                    payload: { error: res.data.error },
+                    payload: { error: res.data.error }
                 });
             }
         }
-    };
-};
+    }
+}
 
 export const isUserLoggedIn = () => {
-    return async (dispatch) => {
-        const token = localStorage.getItem("token");
+    return async dispatch => {
+        const token = localStorage.getItem('token');
         if (token) {
-            const user = JSON.parse(localStorage.getItem("user"));
+            const user = JSON.parse(localStorage.getItem('user'));
             dispatch({
                 type: authConstants.LOGIN_SUCCESS,
                 payload: {
-                    token,
-                    user,
-                },
+                    token, user
+                }
             });
         } else {
             dispatch({
                 type: authConstants.LOGIN_FAILURE,
-                payload: { error: "Failed to login" },
+                payload: { error: 'Failed to login' }
             });
         }
-    };
-};
+    }
+}
 
 export const signout = () => {
-    return async (dispatch) => {
-        dispatch({ type: authConstants.LOGOUT_REQUEST });
-        // localStorage.removeItem('user');
-        // localStorage.removeItem('token');
-        localStorage.clear();
-        dispatch({ type: authConstants.LOGOUT_SUCCESS });
-        //const res = await axios.post(`/admin/signout`);
-        // if(res.status === 200){
+    return async dispatch => {
 
-        // }else{
-        //     dispatch({
-        //         type: authConstants.LOGOUT_FAILURE,
-        //         payload: { error: res.data.error }
-        //     });
-        // }
-    };
-};
+        dispatch({ type: authConstants.LOGOUT_REQUEST });
+        const res = await axios.post(`/admin/signout`);
+
+        if (res.status === 200) {
+            localStorage.clear();
+            dispatch({ type: authConstants.LOGOUT_SUCCESS });
+        } else {
+            dispatch({
+                type: authConstants.LOGOUT_FAILURE,
+                payload: { error: res.data.error }
+            });
+        }
+
+
+    }
+} 
--- client/src/reducers/product.reducer.js
+++ client/src/reducers/product.reducer.js
@@ -1,28 +1,51 @@
-// import { productConstants } from "../actions/constants"
+import { productConstants } from "../actions/constants"
 
-// const initState = {
-//     products: [],
-//     productsByPrice: {
-//         under5k: [],
-//         under10k: [],
-//         under15k: [],
-//         under20k: [],
-//         under30k: []
-//     }
-// }
+const initState = {
+    products: [],
+    productsByPrice: {
+        under5k: [],
+        under10k: [],
+        under15k: [],
+        under20k: [],
+        under30k: []
+    },
+    pageRequest: false,
+    page: {},
+    error: null
+}
 
-// export default (state = initState, action) => {
-//     switch (action.type) {
-//         case productConstants.GET_PRODUCTS_BY_SLUG:
-//             state = {
-//                 ...state,
-//                 products: action.payload.products,
-//                 productsByPrice: {
-//                     ...action.payload.productsByPrice
-//                 }
-//             }
-//             break;
-//     }
+export default (state = initState, action) => {
+    switch (action.type) {
+        case productConstants.GET_PRODUCTS_BY_SLUG:
+            state = {
+                ...state,
+                products: action.payload.products,
+                productsByPrice: {
+                    ...action.payload.productsByPrice
+                }
+            }
+            break;
+        case productConstants.GET_PRODUCT_PAGE_REQUEST:
+            state = {
+                ...state,
+                pageRequest: true
+            }
+            break;
+        case productConstants.GET_PRODUCT_PAGE_SUCCESS:
+            state = {
+                ...state,
+                page: action.payload.page,
+                pageRequest: false
+            }
+            break;
+        case productConstants.GET_PRODUCT_PAGE_FAILURE:
+            state = {
+                ...state,
+                pageRequest: false,
+                error: action.payload.error
+            }
+            break;
+    }
 
-//     return state;
-// } 
+    return state;
+}
